6087083060b32cfa13fc2b71093360dad4be5e1e
madafaker <ahmadlashgari.al@gmail.com>
2016-01-07 19:49:04 +0330

    Create ver.lua

---
+++ plugins/ver.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return 
+  [[ fuck you :D ]]
+end
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows infernal robot version",
+  patterns = {
+    "^!ver$"
+  }, 
+  run = run 
+}
+end
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. 
